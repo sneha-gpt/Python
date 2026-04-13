11ed9349fd0bb3e1690c879ec164447b89e2d205
Sneha Gupta <149265183+sneha-gpt@users.noreply.github.com>
2026-04-13 10:04:15 +0000

    feat: Add more logic and operator task

--- Beginner Level/LogicAndOperators.py
+++ Beginner Level/LogicAndOperators.py
@@ -28,3 +28,55 @@ elif not email.strip():
     print("Email is empty.")
 else:
     print(f"{email.strip()} is verified.")
+
+# Task 4: Check if a user's name is not empty and the age is greater than or equal to 18
+
+name = " Sneha "
+age = 21
+
+if not age or age < 18:
+    print("Not eligible for vote")
+elif not name:
+    print("Name is either None or empty")
+elif not name.strip():
+    print("Name is empty.")
+else:
+    print(f"{name.strip()} is eligible to vote.")
+
+# Task 5: Check if the password is at least 8 characters long and does not contain spaces
+
+password = "asdfghj "
+
+if not password or len(password) < 8 or len(password) != len(password.strip()):
+    print("Password is not secure.")
+else:
+    print("Strong Password!!")
+    
+# Task 6: Check if a user’s email is not empty, contains '@', and ends with '.com'
+
+user_email = "sneha@gmail.com"
+if (not user_email) or ((user_email.count('@') != 1) or (not user_email.endswith(".com"))):
+    print(f"{user_email} is not valid")
+else:
+    print(f"{user_email} is valid")
+
+# Task 7: Check if a username is a string, is not None, and is longer than 5 characters
+
+username = "heyy"
+if (not username) or ((not isinstance(username, str)) or (len(username.strip()) <= 5)):
+    print(f"{username} is Invalid")
+else:
+    print(f"{username} is valid")
+
+# Task 8: Check if the user is either an admin or a moderator, and either they’re not banned or 
+# they’ve verified their email
+
+user_role = "Admin"
+user_banned = False
+email_verified = True
+valid_roles = ["admin", "moderator"]
+
+if ((user_role) and (email_verified)) and ((user_role.lower() in valid_roles) and (not user_banned)):
+    print("Accepted")
+else:
+    print("Rejected")
