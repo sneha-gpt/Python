cdb27d54725862ed7f7b1bd65691017a0c24b1f7
Sneha Gupta <149265183+sneha-gpt@users.noreply.github.com>
2026-04-14 09:45:53 +0000

    feat: Added more loop practice task

--- Beginner Level/Loops.py
+++ Beginner Level/Loops.py
@@ -45,3 +45,36 @@ for file in files:
         break
 else:
     print("All files are CSV")
+
+# Task 5 -> Check whether any filname appears more than once
+
+file_list = ['report.csv',
+             'data.xlsx',
+             'summary.docx',
+             'report.csv',
+             'data.csv']
+
+for file in file_list:
+    if file_list.count(file) > 1:
+        print("Duplicate found")
+        break
+else:
+    print("All files are unique")
+
+# Task 6 -> WAP that kepps asking "Do you agree?" until the user types "yes"
+
+answer = None
+# while answer != "yes":
+#     answer = input("Do you agree?(yes/no)")
+# print("Thank you")
+
+# Task 7 -> In  task 3 only max 3 attempts are allowed
+
+attempt = 0
+while attempt < 3:
+    answer = input("Do you agree?(yes/no)")
+    if(answer == "yes"):
+        break
+    attempt = attempt+1
+else:
+ print("3 Strikes, You are Out!!")
